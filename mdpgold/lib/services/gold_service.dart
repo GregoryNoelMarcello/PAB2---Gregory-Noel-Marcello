f8083110e223d97b828959e4051b128eeb2ad2bb
Gregory <gregorynoelmarcellosoetanto_2327240045@mhs.mdp.ac.id>
2026-04-06 16:49:22 +0700

    mdpgold
    services dan screens

---
+++ mdpgold/lib/services/gold_service.dart
@@ -0,0 +1,11 @@
+
+import "package:firebase_database/firebase_database.dart";
+
+class GoldService {
+  final DatabaseReference database=FirebaseDatabase.instance.ref('harga_emas',);
+
+
+Stream<DatabaseEvent> getPriceList(){
+  return database.onValue;
+}
+}
